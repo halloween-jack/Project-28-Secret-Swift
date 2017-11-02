b096311776fe765420433043fc8e944be5af4a2d
halloween-jack <halloween.jack.dev@gmail.com>
2017-11-03 07:39:26 +0900

    Writing somewhere safe: the iOS keychain

--- Project28/ViewController.swift
+++ Project28/ViewController.swift
@@ -18,11 +18,33 @@ class ViewController: UIViewController {
         let notificationCenter = NotificationCenter.default
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: NSNotification.Name.UIKeyboardWillHide, object: nil)
         notificationCenter.addObserver(self, selector: #selector(adjustForKeyboard), name: NSNotification.Name.UIKeyboardWillChangeFrame, object: nil)
+        
+        notificationCenter.addObserver(self, selector: #selector(saveSecretMessage), name: NSNotification.Name.UIApplicationWillResignActive, object: nil)
+        
+        title = "Nothing to see here"
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func unlockSecretMessage() {
+        secret.isHidden = false
+        title = "Secret stuff!"
+        
+        if let text = KeychainWrapper.standard.string(forKey: "SecretMessage") {
+            secret.text = text
+        }
+    }
+    
+    @objc func saveSecretMessage() {
+        if !secret.isHidden {
+            _ = KeychainWrapper.standard.set(secret.text, forKey: "SecretMessage")
+            secret.resignFirstResponder()
+            secret.isHidden = true
+            title = "Nothing to see here"
+        }
     }
 
     @objc func adjustForKeyboard(notification: Notification) {
@@ -44,6 +66,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func authenticateTapped(_ sender: UIButton) {
+        unlockSecretMessage()
     }
 }
 
